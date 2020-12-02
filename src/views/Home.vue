dd7f4145d3a441216c1ff249a89c7da4e3119d5b
saintnun <saintnun150@gmail.com>
2020-12-02 21:42:16 +0900

    Home.vue 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,49 @@
 <template>
   <v-container>
-    <v-row></v-row>
-    <router-view></router-view>
+    <h1>홈컴포넌트</h1>
+    <v-btn @click="save">
+      <strong>저장</strong>
+      <v-icon>
+        mdi-check
+      </v-icon>
+    </v-btn>
+    <v-btn class="ml-2" @click="readOne">
+      <strong>읽기</strong>
+      <v-icon>
+        mdi-book
+      </v-icon>
+    </v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  methods: {
+    save() {
+      console.log('sdf');
+      // ref => Root 위치
+      this.$firebase.database().ref().child('abcd').set({
+        title: 'abcd',
+        text: 'ttttt'
+      })
+    },
+    read() {
+      // 계속 리스닝하고 있음
+      this.$firebase.database().ref().child('abcd').on('value', (snapShot) => {
+        console.log(snapShot);
+        console.log(snapShot.val());
+      });
+    },
+    async readOne() {
+      const snapshot = await this.$firebase.database().ref().child('abcd').once('value');
+      console.log(snapshot.val());
+    }
+  }
+
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 
 </style>
