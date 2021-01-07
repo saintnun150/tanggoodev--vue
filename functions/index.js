02d09ff0c02bddbbed377334820de17e130f1d08
saintnun <saintnun150@gmail.com>
2021-01-07 19:40:53 +0900

    firebase authentication 사용자 등록  - cloud functions 설정

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,3 @@
-//const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +5,38 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tanggoodev.firebaseio.com"
+});
+
+const db = admin.database();
+
+exports.createUser = functions.region('asia-northeast3').auth.user().onCreate(async user => {
+  const { uid, email, displayName, photoURL } = user;
+  const u = {
+    email,
+    displayName,
+    photoURL,
+    createAt: new Date()
+  };
+  await db
+    .ref("users")
+    .child(uid)
+    .set(u);
+});
+
+exports.deleteUser = functions.region('asia-northeast3').auth.user().onDelete(async user => {
+  const { uid } = user;
+  await db
+    .ref("users")
+    .child(uid)
+    .remove();
+});
