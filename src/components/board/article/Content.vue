4771e146ad2e4666e4e1cb171f65a8ea75f3895d
saintnun <saintnun150@gmail.com>
2021-02-04 20:39:06 +0900

    게시물 좋아요 추가

--- src/components/board/article/Content.vue
+++ src/components/board/article/Content.vue
@@ -34,6 +34,13 @@
       수정일: <display-time :time="article.updatedAt"></display-time>
       </span>
       </v-card-actions>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn icon @click="like">
+          <v-icon left :color="liked ? 'success' : ''">mdi-thumb-up</v-icon>
+          <span>{{article.likeCount}}</span>
+        </v-btn>
+      </v-card-actions>
       <v-divider/>
       <display-comment :article="article" :docRef="ref"></display-comment>
     </v-card>
@@ -61,6 +68,16 @@ export default {
       ref: this.$firebase.firestore().collection('boards').doc(this.boardId).collection('articles').doc(this.articleId),
       unsubscribe: null,
       article: null
+    }
+  },
+  computed: {
+    fireUser() {
+      return this.$store.state.fireUser
+    },
+    liked() {
+      if (!this.fireUser) return false
+      //inclues는 배열에서 해당 argument로 넘긴 값이 속해있는지 아닌지 판별하는 array 내장함수. java의 contain 함수와 비슷
+      return this.article.likeUids.includes(this.fireUser.uid)
     }
   },
   async created() {
@@ -104,6 +121,20 @@ export default {
       const us = this.$route.path.split('/')
       us.pop()
       this.$router.push({path: us.join('/')})
+    },
+    async like() {
+      if (!this.fireUser) throw Error('로그인이 필요합니다')
+      if (this.liked) {
+        await this.ref.update({
+          likeCount: this.$firebase.firestore.FieldValue.increment(-1),
+          likeUids: this.$firebase.firestore.FieldValue.arrayRemove(this.fireUser.uid)
+        })
+      } else {
+        await this.ref.update({
+          likeCount: this.$firebase.firestore.FieldValue.increment(1),
+          likeUids: this.$firebase.firestore.FieldValue.arrayUnion(this.fireUser.uid)
+        })
+      }
     }
   }
 }
